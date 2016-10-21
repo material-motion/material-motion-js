3d332218f1e84c954fe1b23fb0d8ea87b798cdfb
Brenton Simpson <appsforartists@google.com>
2016-10-20 18:01:56 -0700

    [_added] " - present" to copyright dates
    Reviewers: O2 Material Motion, markwei
    
    Reviewed By: O2 Material Motion, markwei
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D1747

--- examples/render.js
+++ examples/render.js
@@ -1,5 +1,5 @@
 /** @license
- *  Copyright 2016 The Material Motion Authors. All Rights Reserved.
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
  *  use this file except in compliance with the License. You may obtain a copy
