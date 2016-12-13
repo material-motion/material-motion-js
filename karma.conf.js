497de3d4a8e5004a70d84a22ae53712e976fb11a
Dennis Jin <dennis@tofuness.com>
2016-12-13 06:06:21 +0100

    Add support for testing specific packages
    Adds two additional ways to test packages.
    `yarn run -- --only runtime` runs tests for a specific package.
    `yarn run -- --grep time` runs a subset of tests that belong to
    packages with the string "time" in their name.

--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,74 @@
+const fs = require('fs');
+const minimist = require('minimist');
+
 module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['mocha'],
-    browsers: ['Chrome', 'ChromeCanary', 'Safari', 'Firefox'],
-    reporters: ['progress'],
-    client: {
-      mocha: {
-        reporter: 'html',
+  // Default config for all packages
+  let opts = {
+      basePath: '',
+      frameworks: ['mocha'],
+      browsers: ['Chrome', 'ChromeCanary', 'Safari', 'Firefox'],
+      reporters: ['progress'],
+      client: {
+        mocha: {
+          reporter: 'html',
+        },
       },
-    },
-    files: [
-      'packages/*/src/**/__tests__/**',
-    ],
-    exclude: [
-      '**/*.map',
-    ],
-    preprocessors: {
-      '**/*.ts': ['webpack'],
-      '**/*.js': ['webpack'],
-    },
-    webpack: {
-      devtool: 'eval',
-      resolve: {
-        extensions: ['.js', '.ts'],
+      exclude: [
+        '**/*.map',
+      ],
+      preprocessors: {
+        '**/*.ts': ['webpack'],
+        '**/*.js': ['webpack'],
       },
-      module: {
-        loaders: [
-          {
-            test: /\.tsx?$/, loader: 'ts-loader?transpileOnly=true',
-          },
-        ],
+      webpack: {
+        devtool: 'eval',
+        stats: 'errors-only',
+        resolve: {
+          extensions: ['.js', '.ts'],
+        },
+        module: {
+          loaders: [
+            {
+              test: /\.tsx?$/, loader: 'ts-loader?transpileOnly=true',
+            },
+          ],
+        },
       },
-    },
-    mime: {
-      'text/x-typescript': ['ts', 'tsx'],
-    },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    singleRun: false,
-    concurrency: Infinity,
-  });
+      webpackMiddleware: {
+        stats: {
+          chunks: false,
+        },
+      },
+      mime: {
+        'text/x-typescript': ['ts', 'tsx'],
+      },
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      singleRun: false,
+      concurrency: Infinity,
+  };
+
+  const argv = minimist(process.argv);
+  if (argv.only) {
+    // Run tests for a specific package
+    if (!fs.existsSync(`./packages/${argv.only}`)) {
+      throw new Error(`"${argv.only}" package does not exist!`);
+    }
+    Object.assign(opts, {
+      files: [`packages/${argv.only}/src/**/__tests__/**`],
+    });
+  } else if (argv.grep) {
+    // Run tests for a subset of all packages by name
+    Object.assign(opts, {
+      files: [`packages/*${argv.grep}*/src/**/__tests__/**`],
+    });
+  } else if (!argv.only && !argv.grep) {
+    // Run all tests
+    Object.assign(opts, {
+      files: ['packages/*/src/**/__tests__/**'],
+    });
+  }
+  config.set(opts);
 };
