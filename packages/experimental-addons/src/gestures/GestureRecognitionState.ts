19de541a9ee1b5ef9e661d9b45144260e764f8b2
Brenton Simpson <appsforartists@google.com>
2017-02-06 16:26:45 -0500

    [added] GestureRecognitionState
    Summary: As defined in https://material-motion.github.io/material-motion/starmap/specifications/primitives/gesture_recognizers/GestureRecognizer#state-api
    
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion, markwei, featherless
    
    Reviewed By: O2 Material Motion, #material_motion, markwei, featherless
    
    Subscribers: markwei
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2653

--- packages/experimental-addons/src/gestures/GestureRecognitionState.ts
+++ packages/experimental-addons/src/gestures/GestureRecognitionState.ts
@@ -14,23 +14,14 @@
  *  under the License.
  */
 
-export * from './types';
+export enum GestureRecognitionState {
+  POSSIBLE,
+  BEGAN,
+  CHANGED,
+  ENDED,
+  CANCELLED,
+  FAILED,
+  RECOGNIZED = ENDED,
+};
 
-export * from './ExperimentalMotionObservable';
-export { default as ExperimentalMotionObservable } from './ExperimentalMotionObservable';
-
-export * from './TogglableProperty';
-export { default as TogglableProperty } from './TogglableProperty';
-
-export * from './PrimitiveKind';
-export { default as PrimitiveKind } from './PrimitiveKind';
-
-export * from './PropertyKind';
-export { default as PropertyKind } from './PropertyKind';
-
-export * from './InputKind';
-export { default as InputKind } from './InputKind';
-
-export * from './gestures';
-
-export default undefined;
+export default GestureRecognitionState;
