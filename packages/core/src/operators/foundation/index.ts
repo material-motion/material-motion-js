5687cbb9c78392e7a046d686d9ab56926b479bed
Brenton Simpson <appsforartists@google.com>
2017-04-03 22:57:54 -0400

    [added] withMap to /operators/foundation
    Summary:
    
    Related: https://github.com/material-motion/material-motion-js/issues/114
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:

--- packages/core/src/operators/foundation/index.ts
+++ packages/core/src/operators/foundation/index.ts
@@ -20,6 +20,11 @@ import {
 } from '../../types';
 
 import {
+  MotionMappable,
+  withMap,
+} from './_map';
+
+import {
   MotionNextOperable,
   withNextOperator,
 } from './_nextOperator';
@@ -28,8 +33,15 @@ export type ObservableWithFoundationalMotionOperators<T> = MotionNextOperable<T>
 
 export function withFoundationalMotionOperators<T, S extends Constructor<Observable<T>>>(superclass: S): S
     & Constructor<ObservableWithFoundationalMotionOperators<T>> {
-  return withNextOperator<T, S>(superclass);
+  // Not sure if withMap needs to be specialized.  Trying results in this error:
+  //     Type 'Constructor<MotionNextOperable<T>> &
+  //     Constructor<MotionMappable<T>>' is not assignable to type 'S &
+  //     Constructor<MotionNextOperable<T>>'.
+  //       Type 'Constructor<MotionNextOperable<T>> &
+  //       Constructor<MotionMappable<T>>' is not assignable to type 'S'.
+  return withMap(withNextOperator<T, S>(superclass));
 }
 
+export * from './_map';
 export * from './_nextOperator';
 
