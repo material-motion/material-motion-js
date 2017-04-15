ce961feb98c894dad94ef55761de980df3c67bf5
Brenton Simpson <appsforartists@google.com>
2017-04-15 15:14:59 -0700

    [added] motionObservable.upperBound(limit)
    Summary: Closes https://github.com/material-motion/material-motion-js/issues/178
    
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3053

---
+++ packages/core/src/operators/upperBound.ts
@@ -0,0 +1,35 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import {
+  Constructor,
+  MotionMappable,
+  Observable,
+} from '../types';
+
+export interface MotionUpperBoundable {
+  upperBound(limit: number): Observable<number>;
+}
+
+export function withUpperBound<T, S extends Constructor<MotionMappable<T>>>(superclass: S): S & Constructor<MotionUpperBoundable<T>> {
+  return class extends superclass implements MotionUpperBoundable {
+    upperBound(limit: number): Observable<number> {
+      return this._map(
+        (value: number) => Math.min(value, limit)
+      );
+    }
+  };
+}
