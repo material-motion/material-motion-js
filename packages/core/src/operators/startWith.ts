d5a5b338e7ed2e2ea4adb839d45c807046a90280
Brenton Simpson <appsforartists@google.com>
2017-04-18 13:21:57 -0700

    [added] motionObservable.startWith(initialValue)
    Closes https://github.com/material-motion/material-motion-js/issues/177

---
+++ packages/core/src/operators/startWith.ts
@@ -0,0 +1,43 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import {
+  Constructor,
+  Observable,
+  ObservableWithFoundationalMotionOperators,
+  Observer,
+} from '../types';
+
+export interface MotionSeedable<T> {
+  startWith(initialValue: T): Observable<T>;
+}
+
+export function withStartWith<T, S extends Constructor<ObservableWithFoundationalMotionOperators<T>>>(superclass: S): S & Constructor<MotionSeedable<T>> {
+  return class extends superclass implements MotionSeedable<T> {
+    startWith(initialValue: T): Observable<T> {
+      const constructor = this.constructor as Constructor<ObservableWithFoundationalMotionOperators<T>>;
+
+      return new constructor(
+        (observer: Observer<T>) => {
+          observer.next(initialValue);
+          const subscription = this.subscribe(observer);
+
+          return subscription.unsubscribe;
+        }
+      )._remember();
+    }
+  };
+}
