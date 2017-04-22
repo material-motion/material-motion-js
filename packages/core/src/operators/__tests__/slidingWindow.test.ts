b9fd0cf13cff3995632e75f32e9f6479dca9497d
Brenton Simpson <appsforartists@google.com>
2017-04-21 17:29:24 -0700

    [added] motionObservable.slidingWindow(length)
    Summary: Closes https://github.com/material-motion/material-motion-js/issues/199
    
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3088

---
+++ packages/core/src/operators/__tests__/slidingWindow.test.ts
@@ -0,0 +1,104 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import { expect } from 'chai';
+
+import {
+  beforeEach,
+  describe,
+  it,
+} from 'mocha-sugar-free';
+
+import {
+  stub,
+} from 'sinon';
+
+declare function require(name: string);
+
+// chai really doesn't like being imported as an ES2015 module; will be fixed in v4
+require('chai').use(
+  require('sinon-chai')
+);
+
+import {
+  createMockObserver,
+} from 'material-motion-testing-utils';
+
+import {
+  MotionObservable,
+} from '../../observables/';
+
+describe('motionObservable.slidingWindow',
+  () => {
+    let stream;
+    let mockObserver;
+    let listener;
+
+    beforeEach(
+      () => {
+        mockObserver = createMockObserver();
+        stream = new MotionObservable(mockObserver.connect);
+        listener = stub();
+      }
+    );
+
+    it(`should not dispatch until the array is the correct length`,
+      () => {
+        stream.slidingWindow(3).subscribe(listener);
+
+        mockObserver.next(1);
+
+        expect(listener).not.to.have.been.called;
+      }
+    );
+
+    it(`should dispatch when the array is the correct length`,
+      () => {
+        stream.slidingWindow(3).subscribe(listener);
+
+        mockObserver.next(1);
+        mockObserver.next(2);
+        mockObserver.next(3);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith([1, 2, 3]);
+      }
+    );
+
+    it(`should remove old items as new ones are received`,
+      () => {
+        stream.slidingWindow(3).subscribe(listener);
+
+        mockObserver.next(1);
+        mockObserver.next(2);
+        mockObserver.next(3);
+        mockObserver.next(4);
+
+        expect(listener).to.have.been.calledTwice.and.to.have.been.calledWith([2, 3, 4]);
+      }
+    );
+
+    it(`should have a default size of 2`,
+      () => {
+        stream.slidingWindow().subscribe(listener);
+
+        mockObserver.next(1);
+        mockObserver.next(2);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith([1, 2]);
+      }
+    );
+  }
+);
