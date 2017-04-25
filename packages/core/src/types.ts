4a0daa0d0d41889217b571889b26cc6d5b2e2dc8
Brenton Simpson <appsforartists@google.com>
2017-04-24 18:01:04 -0700

    [added] motionObservable.velocity(pulse)
    Summary:
    Ported over from the same library that google.com uses internally for its full-screen AMP carousel, so it should be fairly accurate.
    
    Original algorithm courtesy Ryan Fioravanti and Matthew Bolohan.
    
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3107

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -61,6 +61,7 @@ export {
   MotionThresholdRangeable,
   MotionTimestampable,
   MotionUpperBoundable,
+  MotionVelocityMeasurable,
   MotionWindowable,
   ObservableWithFoundationalMotionOperators,
   ObservableWithMotionOperators,
