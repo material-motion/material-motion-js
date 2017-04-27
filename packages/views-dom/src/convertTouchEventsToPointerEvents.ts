8d9a3d39b8d18a5b036f47c339d3a61193e119ae
Brenton Simpson <appsforartists@google.com>
2017-04-26 17:25:53 -0700

    [added] convertTouchEventsToPointerEvents(touchEvent$)
    Summary: Closes https://github.com/material-motion/material-motion-js/issues/211
    
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion, featherless
    
    Reviewed By: O2 Material Motion, #material_motion, featherless
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3120

---
+++ packages/views-dom/src/convertTouchEventsToPointerEvents.ts
@@ -0,0 +1,50 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import {
+  Dict,
+  MotionObservable,
+  PartialPointerEvent,
+} from 'material-motion';
+
+// TouchEvent types from https://developer.apple.com/reference/webkitjs/touchevent/
+// PointerEvent types from https://w3c.github.io/pointerevents/
+const TOUCH_TYPE_TO_POINTER_TYPE: Dict<string> = {
+  'touchstart': 'pointerdown',
+  'touchmove': 'pointermove',
+  'touchend': 'pointerup',
+  'touchcancel': 'pointercancel',
+};
+
+export function convertTouchEventsToPointerEvents(touchEvent$: MotionObservable<TouchEvent>): MotionObservable<PartialPointerEvent> {
+  return touchEvent$._map(
+    ({ type, targetTouches, changedTouches }: TouchEvent) => Array.from(
+      type === 'touchend'
+        ? changedTouches
+        : targetTouches
+    ).map(
+      ({ pageX, pageY, identifier }: Touch) => (
+        {
+          pageX,
+          pageY,
+          pointerId: identifier,
+          type: TOUCH_TYPE_TO_POINTER_TYPE[type]
+        }
+      )
+    )
+  )._flattenIterables();
+}
+export default convertTouchEventsToPointerEvents;
