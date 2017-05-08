23016b3d07032206e56ed21b4ed15b5975610c5f
Brenton Simpson <appsforartists@google.com>
2017-05-08 11:59:29 -0700

    [added] frame$
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3178

---
+++ packages/core/src/__tests__/frame$.test.ts
@@ -0,0 +1,93 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import { expect } from 'chai';
+
+import {
+  after,
+  before,
+  beforeEach,
+  describe,
+  it,
+} from 'mocha-sugar-free';
+
+import {
+  stub,
+} from 'sinon';
+
+declare function require(name: string);
+
+// chai really doesn't like being imported as an ES2015 module; will be fixed in v4
+require('chai').use(
+  require('sinon-chai')
+);
+
+import {
+  createMockObserver,
+  useMockedRAF,
+} from 'material-motion-testing-utils';
+
+import {
+  frame$,
+} from '../timers';
+
+describe('frame$',
+  useMockedRAF(
+    (mockRAF) => {
+      let listener;
+
+      beforeEach(
+        () => {
+          listener = stub();
+        }
+      );
+
+      it('should dispatch at the beginning of each frame',
+        () => {
+          frame$.subscribe(listener);
+
+          mockRAF.step();
+          expect(listener).to.have.been.calledOnce;
+
+          mockRAF.step();
+          expect(listener).to.have.been.calledTwice;
+        }
+      );
+
+      it('should stop dispatching on unsubscription',
+        () => {
+          const subscription = frame$.subscribe(listener);
+
+          mockRAF.step();
+          subscription.unsubscribe();
+          mockRAF.step();
+
+          expect(listener).to.have.been.calledOnce;
+        }
+      );
+
+      it('should not dispatch if unsubscribed from before a frame occurs',
+        () => {
+          const subscription = frame$.subscribe(listener);
+          subscription.unsubscribe();
+          mockRAF.step();
+
+          expect(listener).not.to.have.been.called;
+        }
+      );
+    }
+  )
+);
---
+++ packages/core/src/timers.ts
@@ -0,0 +1,52 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import {
+  MotionObservable,
+} from './observables';
+
+import {
+  ObservableWithMotionOperators,
+  Observer,
+} from './types';
+
+/**
+ * A stream backed by `requestAnimationFrame`, called once per frame with that
+ * frame's timestamp.
+ *
+ * Useful as a pulse for `_debounce` to ensure that UI work only happens once
+ * per frame.  Since no rendering will happen until `requestAnimationFrame` is
+ * called, it should be safe to `_debounce(frame$)` without missing a frame.
+ */
+export const frame$ = new MotionObservable(
+  (observer: Observer<number>) => {
+    let queuedFrameID = 0;
+
+    function queueFrame(frameTimestamp?: number) {
+      if (frameTimestamp) {
+        observer.next(frameTimestamp);
+      }
+
+      queuedFrameID = requestAnimationFrame(queueFrame);
+    }
+
+    queueFrame();
+
+    return () => {
+      cancelAnimationFrame(queuedFrameID);
+    };
+  }
+)._multicast();
