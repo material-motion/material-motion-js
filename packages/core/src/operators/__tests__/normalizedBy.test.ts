c5965fe1700d15138140950dd040492a2521e012
Brenton Simpson <appsforartists@google.com>
2017-06-26 18:50:12 +0200

    [added] motionObservable.normalizedBy(denominator)
    Summary: Closes https://github.com/material-motion/material-motion-js/issues/179
    
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion, featherless
    
    Reviewed By: O2 Material Motion, #material_motion, featherless
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3251

---
+++ packages/core/src/operators/__tests__/normalizedBy.test.ts
@@ -0,0 +1,81 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import { expect } from 'chai';
+
+import {
+  beforeEach,
+  describe,
+  it,
+} from 'mocha-sugar-free';
+
+import {
+  stub,
+} from 'sinon';
+
+declare function require(name: string);
+
+// chai really doesn't like being imported as an ES2015 module; will be fixed in v4
+require('chai').use(
+  require('sinon-chai')
+);
+
+import {
+  createMockObserver,
+} from 'material-motion-testing-utils';
+
+import {
+  MemorylessIndefiniteSubject,
+  MotionObservable,
+} from '../../observables/';
+
+describe('motionObservable.normalizedBy',
+  () => {
+    const denominatorSubject = new MemorylessIndefiniteSubject();
+    let stream;
+    let mockObserver;
+    let listener;
+
+    beforeEach(
+      () => {
+        mockObserver = createMockObserver();
+        stream = new MotionObservable(mockObserver.connect);
+        listener = stub();
+      }
+    );
+
+    it('should divide the incoming value by the denominator and dispatch the result',
+      () => {
+        stream.normalizedBy(5).subscribe(listener);
+
+        mockObserver.next(10);
+
+        expect(listener).to.have.been.calledWith(2);
+      }
+    );
+
+    it('should divide values from a denominator stream and dispatch the result',
+      () => {
+        stream.normalizedBy(denominatorSubject).subscribe(listener);
+
+        mockObserver.next(30);
+        denominatorSubject.next(5);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(6);
+      }
+    );
+  }
+);
---
+++ packages/core/src/operators/normalizedBy.ts
@@ -0,0 +1,40 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import {
+  Constructor,
+  MotionReactiveMappable,
+  Observable,
+  ObservableWithMotionOperators,
+} from '../types';
+
+export interface MotionNormalizable {
+  normalizedBy(denominator$: number | Observable<number>): ObservableWithMotionOperators<number>;
+}
+
+export function withNormalizedBy<T, S extends Constructor<MotionReactiveMappable<T>>>(superclass: S): S & Constructor<MotionNormalizable> {
+  return class extends superclass implements MotionNormalizable {
+     /**
+     * Divides the incoming value by the denominator and dispatches the result.
+     */
+   normalizedBy(denominator$: number | Observable<number>): ObservableWithMotionOperators<number> {
+      return (this as any as ObservableWithMotionOperators<number>)._reactiveMap(
+        (value: number, denominator: number) => value / denominator,
+        denominator$
+      );
+    }
+  };
+}
