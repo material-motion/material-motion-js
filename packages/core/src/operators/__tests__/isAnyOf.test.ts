123550132c8306f73882628ca73a101ddcff0268
Brenton Simpson <appsforartists@google.com>
2017-07-04 17:31:30 +0200

    [added] motionObservable.isAnyOf(possibilities)
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3262

---
+++ packages/core/src/operators/__tests__/isAnyOf.test.ts
@@ -0,0 +1,81 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import { expect } from 'chai';
+
+import {
+  beforeEach,
+  describe,
+  it,
+} from 'mocha-sugar-free';
+
+import {
+  stub,
+} from 'sinon';
+
+declare function require(name: string);
+
+// chai really doesn't like being imported as an ES2015 module; will be fixed in v4
+require('chai').use(
+  require('sinon-chai')
+);
+
+import {
+  createMockObserver,
+} from 'material-motion-testing-utils';
+
+import {
+  MemorylessIndefiniteSubject,
+  MotionObservable,
+} from '../../observables/';
+
+describe('motionObservable.isAnyOf',
+  () => {
+    let subject;
+    let stream;
+    let mockObserver;
+    let listener;
+
+    beforeEach(
+      () => {
+        mockObserver = createMockObserver();
+        subject = new MemorylessIndefiniteSubject();
+        stream = new MotionObservable(mockObserver.connect);
+        listener = stub();
+      }
+    );
+
+    it('should dispatch true when the upstream values matches a possibility',
+      () => {
+        stream.isAnyOf([1, 2, 3]).subscribe(listener);
+
+        mockObserver.next(2);
+
+        expect(listener).to.have.been.calledWith(true);
+      }
+    );
+
+    it('should dispatch false when the upstream values matches a possibility',
+      () => {
+        stream.isAnyOf([1, 2, 3]).subscribe(listener);
+
+        mockObserver.next(4);
+
+        expect(listener).to.have.been.calledWith(false);
+      }
+    );
+  }
+);
