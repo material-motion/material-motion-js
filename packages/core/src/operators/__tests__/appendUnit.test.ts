aff4d5f43dbfe1f3699faba05df421d4154d7efb
Brenton Simpson <appsforartists@google.com>
2017-07-19 10:34:50 -0700

    [added] motionObservable.appendUnit(unit)
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3269

---
+++ packages/core/src/operators/__tests__/appendUnit.test.ts
@@ -0,0 +1,63 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import { expect, use as useInChai } from 'chai';
+import * as sinonChai from 'sinon-chai';
+useInChai(sinonChai);
+
+import {
+  beforeEach,
+  describe,
+  it,
+} from 'mocha-sugar-free';
+
+import {
+  stub,
+} from 'sinon';
+
+import {
+  createMockObserver,
+} from 'material-motion-testing-utils';
+
+import {
+  MotionObservable,
+} from '../../observables/';
+
+describe('motionObservable.appendUnit',
+  () => {
+    let stream;
+    let mockObserver;
+    let listener;
+
+    beforeEach(
+      () => {
+        mockObserver = createMockObserver();
+        stream = new MotionObservable(mockObserver.connect);
+        listener = stub();
+      }
+    );
+
+    it('should append the given unit to the upstream values',
+      () => {
+        stream.appendUnit('px').subscribe(listener);
+
+        mockObserver.next(50);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith('50px');
+      }
+    );
+  }
+);
