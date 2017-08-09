80a65649b70f8301bded87684633363a6b198100
Brenton Simpson <appsforartists@google.com>
2017-08-08 17:34:11 -0700

    [fixed] demo server
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3294

--- packages/demos-react/.pundle.js
+++ packages/demos-react/.pundle.js
@@ -41,7 +41,7 @@ module.exports = {
           extensions,
         },
         transformer: {
-          extensions: ['ts', 'tsx'],
+          extensions,
           config: {
             compilerOptions: Object.assign(
               {},
