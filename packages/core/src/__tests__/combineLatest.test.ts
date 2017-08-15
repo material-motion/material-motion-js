0e1f46fa3f21ab93581f5ab8d7c0c88fc72c2960
Brenton Simpson <appsforartists@google.com>
2017-08-15 16:36:00 -0700

    [moved] combineLatest from experimental-addons to core
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3311

---
+++ packages/core/src/__tests__/combineLatest.test.ts
@@ -0,0 +1,151 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import { expect, use as useInChai } from 'chai';
+import * as sinonChai from 'sinon-chai';
+useInChai(sinonChai);
+
+import {
+  beforeEach,
+  describe,
+  it,
+} from 'mocha-sugar-free';
+
+import {
+  stub,
+} from 'sinon';
+
+import {
+  MemorylessIndefiniteSubject,
+} from '../observables';
+
+import {
+  combineLatest,
+} from '../combineLatest';
+
+describe('combineLatest',
+  () => {
+    let subject1;
+    let subject2;
+    let listener;
+
+    beforeEach(
+      () => {
+        subject1 = new MemorylessIndefiniteSubject();
+        subject2 = new MemorylessIndefiniteSubject();
+        listener = stub();
+      }
+    );
+
+    it('should dispatch a dictionary of values when it receives a dictionary',
+      () => {
+        combineLatest({ a: subject1, b: subject2 }).subscribe(listener);
+
+        subject1.next(2);
+        subject2.next(3);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith({ a: 2, b: 3 });
+      }
+    );
+
+    it('should dispatch an array of values when it receives an array',
+      () => {
+        combineLatest([ subject1, subject2 ]).subscribe(listener);
+
+        subject1.next(2);
+        subject2.next(3);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith([ 2, 3 ]);
+      }
+    );
+
+    it('should dispatch each time it receives a new value when it receives a dictionary',
+      () => {
+        combineLatest({ a: subject1, b: subject2 }).subscribe(listener);
+
+        subject1.next(1);
+        subject2.next(3);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith({ a: 1, b: 3 });
+
+        subject1.next(2);
+        expect(listener).to.have.been.calledTwice.and.to.have.been.calledWith({ a: 2, b: 3 });
+
+        subject2.next(2);
+        expect(listener).to.have.been.calledThrice.and.to.have.been.calledWith({ a: 2, b: 2 });
+      }
+    );
+
+    it('should dispatch each time it receives a new value when it receives an array',
+      () => {
+        combineLatest([ subject1, subject2 ]).subscribe(listener);
+
+        subject1.next(1);
+        subject2.next(3);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith([ 1, 3 ]);
+
+        subject1.next(2);
+        expect(listener).to.have.been.calledTwice.and.to.have.been.calledWith([ 2, 3 ]);
+
+        subject2.next(2);
+        expect(listener).to.have.been.calledThrice.and.to.have.been.calledWith([ 2, 2 ]);
+      }
+    );
+
+    it('should passthrough constants when it receives a dictionary',
+      () => {
+        combineLatest({ a: subject1, b: subject2, c: 1 }).subscribe(listener);
+
+        subject1.next(2);
+        subject2.next(3);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith({ a: 2, b: 3, c: 1 });
+      }
+    );
+
+    it('should passthrough constants when it receives an array',
+      () => {
+        combineLatest([ 1, subject1, subject2 ]).subscribe(listener);
+
+        subject1.next(2);
+        subject2.next(3);
+
+        expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith([ 1, 2, 3 ]);
+      }
+    );
+
+    it('should not dispatch until it receives a value from each item in the dictionary',
+      () => {
+        combineLatest({ a: subject1, b: subject2 }).subscribe(listener);
+
+        subject1.next(1);
+
+        expect(listener).not.to.have.been.called;
+      }
+    );
+
+    it('should not dispatch until it receives a value from each item in the array',
+      () => {
+        combineLatest([ subject1, subject2 ]).subscribe(listener);
+
+        subject1.next(1);
+
+        expect(listener).not.to.have.been.called;
+      }
+    );
+  }
+);
---
+++ packages/core/src/combineLatest.ts
@@ -0,0 +1,82 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import {
+  MotionObservable,
+} from './observables/proxies';
+
+import {
+  Dict,
+  Observer,
+  Subscription,
+} from './types';
+
+import {
+  isIterable,
+  isObservable,
+} from './typeGuards';
+
+export function combineLatest<T extends Dict<any> | Iterable<any>>(streams: T): MotionObservable<T> {
+  return new MotionObservable(
+    (observer: Observer<T>) => {
+      const outstandingKeys = new Set(Object.keys(streams));
+
+      let nextValue: T = {};
+
+      if (isIterable(streams)) {
+        nextValue = [];
+      }
+
+      const subscriptions: Dict<Subscription> = {};
+
+      outstandingKeys.forEach(checkKey);
+
+      function checkKey(key: string | number) {
+        const maybeStream: any = streams[key];
+        if (isObservable(maybeStream)) {
+          subscriptions[key] = maybeStream.subscribe(
+            (value: any) => {
+              outstandingKeys.delete(key);
+
+              nextValue[key] = value;
+              dispatchNextValue();
+            }
+          );
+        } else {
+          outstandingKeys.delete(key);
+
+          nextValue[key] = maybeStream;
+          dispatchNextValue();
+        }
+      }
+
+      function dispatchNextValue() {
+        if (!outstandingKeys.size) {
+          observer.next(nextValue);
+        }
+      }
+
+      dispatchNextValue();
+
+      return function disconnect() {
+        Object.values(subscriptions).forEach(
+          subscription => subscription.unsubscribe()
+        );
+      };
+    }
+  );
+}
+
