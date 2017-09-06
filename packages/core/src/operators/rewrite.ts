a17c3f0b255e764b2e019fa4dc0fde879fa1f786
Brenton Simpson <appsforartists@google.com>
2017-09-05 19:16:27 -0700

    [refactored] rewrite to have options
    Summary:
    Not sure what the right pattern is for operator APIs.  Some (like `rewriteTo`) will likely never need options, so requiring every operator to use the named args pattern would be noisey, but consistent.  Passing an options dict as the last arg is messier, but avoids cluttering the APIs that don't need options.
    
    For now, I care more about functionality than API design, so I'm using an options dict.
    
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3315

--- packages/core/src/operators/rewrite.ts
+++ packages/core/src/operators/rewrite.ts
@@ -27,15 +27,18 @@ import {
   ObservableWithMotionOperators,
 } from '../types';
 
+export type RewritableOptions<U> = {
+  defaultValue?: U | symbol,
+};
 export interface MotionRewritable<T> {
-  rewrite<U, R extends U | Observable<U>>(dict: Dict<R> | Map<T, R>, defaultValue?: U | symbol): ObservableWithMotionOperators<U>;
+  rewrite<U, R extends U | Observable<U>>(dict: Dict<R> | Map<T, R>, options: RewritableOptions<U>): ObservableWithMotionOperators<U>;
 }
 
-const SUPPRESS_FAILURES = Symbol();
+export const SUPPRESS_FAILURES = Symbol();
 
 export function withRewrite<T, S extends Constructor<MotionReactiveNextOperable<T>>>(superclass: S): S & Constructor<MotionRewritable<T>> {
   return class extends superclass implements MotionRewritable<T> {
-    rewrite<U, R extends U | Observable<U>>(dict: Dict<R> | Map<T, R>, defaultValue: U | symbol = SUPPRESS_FAILURES): ObservableWithMotionOperators<U> {
+    rewrite<U, R extends U | Observable<U>>(dict: Dict<R> | Map<T, R>, { defaultValue = SUPPRESS_FAILURES }: RewritableOptions<U> = {}): ObservableWithMotionOperators<U> {
       let keys: Iterable<string> | Iterable<T>;
       let values: Iterable<U | Observable<U>>;
       let castKeysToStrings = false;
