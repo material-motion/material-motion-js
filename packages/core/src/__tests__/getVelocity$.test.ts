c1095074603f0cff9cf1aabe35b7a6d7f42587fa
Brenton Simpson <appsforartists@google.com>
2017-10-07 20:25:44 -0700

    [refactored] velocity operator to getVelocity$
    Summary: The existing operator presumed the incoming values were measured in px/ms.  This diff makes that configurable, and separates the velocity calculation into its own function.  A function feels more fitting for this functionality than an operator.
    
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3420

---
+++ packages/core/src/__tests__/getVelocity$.test.ts
@@ -0,0 +1,275 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import { expect, use as useInChai } from 'chai';
+import * as sinonChai from 'sinon-chai';
+useInChai(sinonChai);
+
+import {
+  beforeEach,
+  describe,
+  it,
+} from 'mocha-sugar-free';
+
+import {
+  stub,
+} from 'sinon';
+
+import {
+  Performance,
+  useMockedPerformance
+} from 'material-motion-testing-utils';
+
+import {
+  MemorylessMotionSubject,
+} from '../observables/';
+
+import {
+  DEFAULT_VELOCITY,
+  MAXIMUM_AGE,
+  MAXIMUM_VELOCITY,
+  getVelocity$,
+} from '../getVelocity$';
+
+describe('getVelocity$',
+  useMockedPerformance(
+    (mockPerformance) => {
+      let value$;
+      let pulse$;
+      let listener;
+
+      beforeEach(
+        () => {
+          value$ = new MemorylessMotionSubject();
+          pulse$ = new MemorylessMotionSubject();
+          listener = stub();
+        }
+      );
+
+      it(`should not dispatch unless pulse$ does`,
+        () => {
+          getVelocity$({ value$, pulse$ }).subscribe(listener);
+
+          value$.next(0);
+          value$.next(1);
+          value$.next(2);
+
+          expect(listener).not.to.have.been.called;
+        }
+      );
+
+      it(`should dispatch 0 if pulse$ dispatches before it's received values from upstream`,
+        () => {
+          getVelocity$({ value$, pulse$ }).subscribe(listener);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(0);
+        }
+      );
+
+      it(`should dispatch 0 when there is only one recent data point`,
+        () => {
+          getVelocity$({ value$, pulse$ }).subscribe(listener);
+
+          value$.next(100);
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(0);
+        }
+      );
+
+      it(`should calculate velocity for reasonable px / ms rates`,
+        () => {
+          getVelocity$({ value$, pulse$ }).subscribe(listener);
+
+          value$.next(1);
+          mockPerformance.increment(5);
+          value$.next(7);
+          mockPerformance.increment(5);
+          value$.next(11);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(1);
+        }
+      );
+
+      it(`should support negative velocities`,
+        () => {
+          getVelocity$({ value$, pulse$ }).subscribe(listener);
+
+          value$.next(-1);
+          mockPerformance.increment(5);
+          value$.next(-7);
+          mockPerformance.increment(5);
+          value$.next(-11);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(-1);
+        }
+      );
+
+      it(`should dispatch defaultVelocity for super-fast velocities with only 2 data points`,
+        () => {
+          const defaultVelocity = 3;
+          getVelocity$({ value$, pulse$, defaultVelocity }).subscribe(listener);
+
+          value$.next(7);
+          mockPerformance.increment(20);
+          value$.next(1000);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(defaultVelocity);
+        }
+      );
+
+      it(`should use the correct direction for defaultVelocity`,
+        () => {
+          const defaultVelocity = 3;
+          getVelocity$({ value$, pulse$, defaultVelocity }).subscribe(listener);
+
+          value$.next(-7);
+          mockPerformance.increment(20);
+          value$.next(-1000);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(-defaultVelocity);
+        }
+      );
+
+      it(`should dispatch maximumVelocity for super-fast velocities with at least 3 data points`,
+        () => {
+          const maximumVelocity = 7;
+          getVelocity$({ value$, pulse$, maximumVelocity }).subscribe(listener);
+
+          value$.next(1);
+          mockPerformance.increment(1);
+          value$.next(3);
+          mockPerformance.increment(1);
+          value$.next(6);
+          mockPerformance.increment(1);
+          value$.next(9);
+          mockPerformance.increment(1);
+          value$.next(100);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(maximumVelocity);
+        }
+      );
+
+      it(`should use the correct direction for maximumVelocity`,
+        () => {
+          const maximumVelocity = 7;
+          getVelocity$({ value$, pulse$, maximumVelocity }).subscribe(listener);
+
+          value$.next(-1);
+          mockPerformance.increment(1);
+          value$.next(-3);
+          mockPerformance.increment(1);
+          value$.next(-6);
+          mockPerformance.increment(1);
+          value$.next(-9);
+          mockPerformance.increment(1);
+          value$.next(-100);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(-maximumVelocity);
+        }
+      );
+
+      it(`should ignore trailing values older than MAXIMUM_AGE`,
+        () => {
+          const maximumVelocity = 7;
+          getVelocity$({ value$, pulse$, maximumVelocity }).subscribe(listener);
+
+          value$.next(1);
+          mockPerformance.increment(MAXIMUM_AGE);
+          value$.next(7);
+          mockPerformance.increment(10);
+          value$.next(10);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(.3);
+        }
+      );
+
+      it(`should ignore values that aren't going in the current direction`,
+        () => {
+          getVelocity$({ value$, pulse$ }).subscribe(listener);
+
+          value$.next(7);
+          mockPerformance.increment(10);
+          value$.next(1);
+          mockPerformance.increment(10);
+          value$.next(11);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(1);
+        }
+      );
+
+      it(`should use the most recent 5 values`,
+        () => {
+          getVelocity$({ value$, pulse$ }).subscribe(listener);
+
+          value$.next(2);
+          mockPerformance.increment(16);
+          value$.next(4);
+          mockPerformance.increment(16);
+          value$.next(8);
+          mockPerformance.increment(16);
+          value$.next(16);
+          mockPerformance.increment(16);
+          value$.next(32);
+          mockPerformance.increment(16);
+          value$.next(64);
+          mockPerformance.increment(16);
+          value$.next(128);
+          mockPerformance.increment(16);
+          value$.next(256);
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith(3.75);
+        }
+      );
+
+      it(`should dispatch a point if the upstream values are points`,
+        () => {
+          getVelocity$({ value$, pulse$ }).subscribe(listener);
+
+          value$.next({ x: 0, y: 0 });
+          mockPerformance.increment(16);
+          value$.next({ x: 10, y: 20 });
+          mockPerformance.increment(16);
+          value$.next({ x: 32, y: 64 });
+
+          pulse$.next();
+
+          expect(listener).to.have.been.calledOnce.and.to.have.been.calledWith({ x: 1, y: 2 });
+        }
+      );
+    }
+  )
+);
---
+++ packages/core/src/getVelocity$.ts
@@ -0,0 +1,182 @@
+/** @license
+ *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+import {
+  MotionObservable,
+} from './observables/proxies';
+
+import {
+  createPlucker,
+} from './operators/pluck';
+
+import {
+  Constructor,
+  Observable,
+  ObservableWithMotionOperators,
+  Observer,
+  Point2D,
+  Timestamped,
+} from './types';
+
+import {
+  isPoint2D,
+} from './typeGuards';
+
+// These constants are exported for testing.
+export const MAXIMUM_AGE = 250;
+export const MAXIMUM_INCOMING_DISPATCHES = 5;
+
+type NumericPlucker = (value: any) => number;
+
+const pluckTimestamp = createPlucker('timestamp');
+const pluckValue = createPlucker('value');
+const pluckX = createPlucker('value.x');
+const pluckY = createPlucker('value.y');
+
+export type GetVelocity$Args<U extends number | Point2D> = {
+  value$: ObservableWithMotionOperators<U>,
+  pulse$: ObservableWithMotionOperators<any>,
+  maximumVelocity?: number,
+  defaultVelocity?: number,
+};
+
+/**
+ * Computes the velocity of an incoming stream and dispatches the result.
+ * Velocity's denominator is in milliseconds; if the incoming stream is
+ * measured in pixels, the resulting stream will be in pixels / millisecond.
+ *
+ * Velocity is computed by watching the trailing 250ms of up to 5 dispatches
+ * and measuring the distance between the longest pair of events moving in
+ * the current direction.  This approach is more resiliant to anomolous data
+ * than a simple (nextPosition - prevPosition) / (nextTime - prevTime).
+ *
+ * If `pulse` is supplied, `velocity(pulse)` will only dispatch values when
+ * `pulse` dispatches a value.  This is useful for ensuring that velocity
+ * is only calculated when it will be used.
+ */
+export function getVelocity$<U extends number | Point2D>({
+  value$,
+  pulse$,
+
+  // These values are borrowed from Matthew Bolohan's drag implementation (which
+  // presumes the units are "px / ms").
+  maximumVelocity = 5,
+  defaultVelocity = 1,
+}: GetVelocity$Args<U>): ObservableWithMotionOperators<U> {
+  return new MotionObservable(
+    (observer: Observer<U>) => {
+      let records: Array<Timestamped<U>> = [];
+
+      const trailingSubscription = value$.timestamp()._slidingWindow({ size: MAXIMUM_INCOMING_DISPATCHES }).subscribe(
+        nextRecords => records = nextRecords
+      );
+
+      const pulseSubscription = pulse$.timestamp().subscribe(
+        ({ timestamp: now }) => {
+          const recentRecords = records.filter(
+            ({ timestamp }) => now - timestamp < MAXIMUM_AGE
+          );
+
+          if (records.length && isPoint2D(records[0].value)) {
+            observer.next({
+              x: calculateVelocity<Timestamped<U>>({ records: recentRecords, pluckValue: pluckX, pluckTimestamp, maximumVelocity, defaultVelocity }),
+              y: calculateVelocity<Timestamped<U>>({ records: recentRecords, pluckValue: pluckY, pluckTimestamp, maximumVelocity, defaultVelocity }),
+            } as U);
+          } else {
+            observer.next(
+              calculateVelocity<Timestamped<U>>({ records: recentRecords, pluckValue, pluckTimestamp, maximumVelocity, defaultVelocity }) as U
+            );
+          }
+        }
+      );
+
+      return () => {
+        trailingSubscription.unsubscribe();
+        pulseSubscription.unsubscribe();
+      };
+    }
+  );
+};
+
+type CalculateVelocityArgs<T> = {
+  records: Array<T>,
+  pluckValue: NumericPlucker,
+  pluckTimestamp: NumericPlucker,
+  maximumVelocity: number,
+  defaultVelocity: number,
+};
+
+/**
+ * Computes velocity using the oldest value that's moving in the current
+ * direction.
+ *
+ * It walks backwards over an array of records and makes sure that each segment
+ * is going in the same direction (incrementing or decrementing) as the last
+ * segment.  When it detects a change in direction (or has checked the whole
+ * array), it returns the velocity between the newest value and the oldest
+ * moving in the same direction.
+ */
+function calculateVelocity<T>({ records, pluckValue, pluckTimestamp, maximumVelocity, defaultVelocity }: CalculateVelocityArgs<T>): number {
+  if (records.length < 2) {
+    return 0;
+  }
+
+  let velocity: number = 0;
+
+  // Backing over the array pairwise, `next` is the newer value and `prev` is
+  // the older value.  `last` is always the final value.
+
+  const lastIndex = records.length - 1;
+  const lastValue = pluckValue(records[lastIndex]);
+  const lastTimestamp = pluckTimestamp(records[lastIndex]);
+
+  let prevValue: number;
+  let prevTimestamp: number;
+  let nextValue: number = lastValue;
+  let nextTimestamp: number = lastTimestamp;
+
+  for (let i = lastIndex - 1; i >= 0; i--) {
+    prevValue = pluckValue(records[i]);
+    prevTimestamp = pluckTimestamp(records[i]);
+
+    const averageVelocity = (lastValue - prevValue) / (lastTimestamp - prevTimestamp);
+    const pairwiseVelocity = (nextValue - prevValue) / (nextTimestamp - prevTimestamp);
+
+    if (velocity !== 0 && pairwiseVelocity > 0 !== velocity > 0) {
+      break;
+    } else {
+      velocity = averageVelocity;
+    }
+
+    nextValue = prevValue;
+    nextTimestamp = prevTimestamp;
+  }
+
+  if (Math.abs(velocity) > maximumVelocity) {
+    // If there isn't enough data to trust our accuracy (perhaps the main thread
+    // was blocked), use the default velocity.
+
+    const direction = velocity / Math.abs(velocity);
+
+    if (records.length < 3) {
+      velocity = direction * defaultVelocity;
+    } else {
+      velocity = direction * maximumVelocity;
+    }
+  }
+
+  return velocity;
+}
